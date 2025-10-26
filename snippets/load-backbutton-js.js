a2fe3e5c945fd1104f9d29c149a9e1013ec8c4c5
Srishty Gajbhiye <drsrishtyg@gmail.com>
2025-10-26 16:55:17 +0530

    ⏪ Revert: delete loader snippet

--- snippets/load-backbutton-js.js
+++ snippets/load-backbutton-js.js
@@ -1,7 +1,1 @@
-// include autohide script
-(function(){
-  const s=document.createElement('script');
-  s.src='./scripts/backbutton-autohide.js';
-  s.defer=true;
-  document.head.appendChild(s);
-})();
+// removed
