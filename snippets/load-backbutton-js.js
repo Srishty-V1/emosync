a58525f240a1144cd11c595e2de55965e310d135
Srishty Gajbhiye <drsrishtyg@gmail.com>
2025-10-26 16:47:10 +0530

    🔗 Load backbutton autohide JS from app.js

---
+++ snippets/load-backbutton-js.js
@@ -0,0 +1,7 @@
+// include autohide script
+(function(){
+  const s=document.createElement('script');
+  s.src='./scripts/backbutton-autohide.js';
+  s.defer=true;
+  document.head.appendChild(s);
+})();
